e884adb497f79fd9e75f3cdc2db402b66ce9b248
feridekrpnr <feride.karpinar8@gmail.com>
2022-07-29 18:18:39 +0300

    Düzenleme yapıldı

---
+++ namingConventions/namingConventions/src/business/concretes/ProductManager.java
@@ -0,0 +1,12 @@
+package business.concretes;
+
+public class ProductManager {
+    int enBuyukSayi;
+
+    public ProductManager() {
+        enBuyukSayi = 10;
+    }
+    public void getAllByCategoryId(int categoryId) {
+        int enKucukSayi;
+    }
+}
